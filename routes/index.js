0cc87e5cb73a153524691f7877a0e4450881d0df
eigakow <iga.kowalska@gmail.com>
2017-06-26 10:53:02 +0200

    release 0.0.2

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.render('index');
+});
 
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+router.get('/api/whoami/', function(req, res) {
+
+  //var ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress).match(/(^.*?),/)[1];
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+  if (/^.*?,/.test(ip)) { ip = ip.match(/(^.*?),/)[1]}
   var lang = req.headers['accept-language'].match(/,([A-z]*);/)[1];
   var os = req.headers['user-agent'].match(/\((.*?)\)/)[1];
 
   //console.log(ip, lang, os);
   res.set('Content-Type', 'application/json');
   res.json({ "ip": ip, "language": lang, "os": os});
-
-
 });
 
 module.exports = router;
