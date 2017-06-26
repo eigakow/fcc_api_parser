e6073cd27391ecb5f2641f596d5aeefa6a20d467
eigakow <iga.kowalska@gmail.com>
2017-06-26 10:27:01 +0200

    initial release

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var lang = req.headers['accept-language'].match(/,([A-z]*);/)[1];
+  var os = req.headers['user-agent'].match(/\((.*?)\)/)[1];
+
+  //console.log(ip, lang, os);
+  res.set('Content-Type', 'application/json');
+  res.json({ "ip": ip, "language": lang, "os": os});
+
+
 });
 
 module.exports = router;
